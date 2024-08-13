977c5e427dca9753353524db1daaa38110500504
Geoffrey.C <cycjimmy@gmail.com>
2024-08-13 14:30:16 +0800

    feat: perfect type file

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,28 @@
-declare module '@cycjimmy/vue-h5-audio-controls';
+import {DefineComponent} from 'vue'
+
+declare module '@cycjimmy/vue-h5-audio-controls' {
+  interface Methods {
+    play(): void;
+
+    pause(): void;
+
+    stop(): void;
+
+    changeButtonUI(): void;
+
+    isPlaying(): boolean;
+  }
+
+  const H5AudioControls: DefineComponent<{
+    src: string;
+    position: string;
+    positionType: string;
+    buttonSize: string;
+    iconSize: string;
+    playIcon: string;
+    pauseIcon: string;
+    autoPlay: boolean;
+  }, {}, Methods>;
+
+  export default H5AudioControls;
+}
